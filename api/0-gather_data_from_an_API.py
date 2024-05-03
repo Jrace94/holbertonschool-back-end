0476ad10b87032f633b49ae77e2bab48df686263
Jrace94 <jfra.ing28@gmail.com>
2024-05-03 01:02:35 -0500

    Task 0: Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,19 +6,20 @@ import sys
 done_tasks = []
 url = "https://jsonplaceholder.typicode.com"
 
-response_todos = requests.get(f"{url}/todos?userid={sys.argv[1]}")
-response_users = requests.get(f"{url}/users/{sys.argv[1]}")
+if __name__ == "__main__":
+    response_todos = requests.get(f"{url}/todos?userid={sys.argv[1]}")
+    response_users = requests.get(f"{url}/users/{sys.argv[1]}")
 
-todos = response_todos.json()
-user = response_users.json()
+    todos = response_todos.json()
+    user = response_users.json()
 
-for todo in todos:
-    if todo.get("completed"):
-        done_tasks.append(todo)
+    for todo in todos:
+        if todo.get("completed"):
+            done_tasks.append(todo)
 
-name = user.get("name")
-tasks = len(done_tasks)
-print(f"Employee {name} is done with tasks({tasks}/{len(todos)}):")
+    name = user.get("name")
+    tasks = len(done_tasks)
+    print(f"Employee {name} is done with tasks({tasks}/{len(todos)}):")
 
-for todo in done_tasks:
-    print("\t {}".format(todo.get("title")))
+    for todo in done_tasks:
+        print("\t {}".format(todo.get("title")))
