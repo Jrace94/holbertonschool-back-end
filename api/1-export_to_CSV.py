2ec438084c46b78ed60f3e6798f106194b4e097b
Jrace94 <jfra.ing28@gmail.com>
2024-05-03 03:50:50 -0500

    Task 1: Export to CSV

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module that exports a CSV file"""
+import csv
+import sys
+
+response_API = __import__("0-gather_data_from_an_API")
+
+url = response_API.url
+todos, user = response_API.response(sys.argv[1])
+
+
+if __name__ == "__main__":
+    with open("USER_ID.csv", "w") as USER_ID:
+        user_writer = csv.writer(
+            USER_ID,
+            delimiter=",",
+            quoting=csv.QUOTE_ALL,
+        )
+        for todo in todos:
+            user_writer.writerow(
+                [
+                    todo["userId"],
+                    user["name"],
+                    todo["completed"],
+                    todo["title"],
+                ]
+            )
